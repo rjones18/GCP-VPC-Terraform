c60f15666848fdeb3e0e1ff2ef103967a9e3b6ac
rjones18 <regjones16@outlook.com>
2023-03-21 01:26:14 +0000

    Initial Commit

---
+++ vpc.tf
@@ -0,0 +1,11 @@
+resource "google_compute_network" "vpc_network" {
+  name                    = "project-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "vpc_subnet" {
+  name          = "my-subnet"
+  ip_cidr_range = "10.0.0.0/24"
+  network       = google_compute_network.vpc_network.self_link
+  region        = "us-central1"
+}
