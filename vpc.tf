b47b629a040c1ce02bbcbfe3fa843d3f6e97c2f4
rjones18 <regjones16@outlook.com>
2023-03-22 20:43:19 +0000

    Update subnet info

--- vpc.tf
+++ vpc.tf
@@ -3,44 +3,44 @@ resource "google_compute_network" "vpc_network" {
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "subnet-1" {
-  name          = "subnet-1"
+resource "google_compute_subnetwork" "web-subnet-1" {
+  name          = "web-subnet-1"
   ip_cidr_range = "10.0.1.0/24"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.self_link
 }
 
-resource "google_compute_subnetwork" "subnet-2" {
-  name          = "subnet-2"
+resource "google_compute_subnetwork" "web-subnet-2" {
+  name          = "web-subnet-2"
   ip_cidr_range = "10.0.2.0/24"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.self_link
 }
 
-resource "google_compute_subnetwork" "subnet-3" {
-  name          = "subnet-3"
-  ip_cidr_range = "10.0.3.0/24"
+resource "google_compute_subnetwork" "app-subnet-1" {
+  name          = "app-subnet-1"
+  ip_cidr_range = "10.0.10.0/24"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.self_link
 }
 
-resource "google_compute_subnetwork" "subnet-4" {
-  name          = "subnet-4"
-  ip_cidr_range = "10.0.4.0/24"
+resource "google_compute_subnetwork" "app-subnet-2" {
+  name          = "app-subnet-2"
+  ip_cidr_range = "10.0.11.0/24"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.self_link
 }
 
-resource "google_compute_subnetwork" "subnet-5" {
-  name          = "subnet-5"
-  ip_cidr_range = "10.0.5.0/24"
+resource "google_compute_subnetwork" "data-subnet-1" {
+  name          = "data-subnet-1"
+  ip_cidr_range = "10.0.20.0/24"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.self_link
 }
 
-resource "google_compute_subnetwork" "subnet-6" {
-  name          = "subnet-6"
-  ip_cidr_range = "10.0.6.0/24"
+resource "google_compute_subnetwork" "data-subnet-2" {
+  name          = "data-subnet-2"
+  ip_cidr_range = "10.0.21.0/24"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.self_link
 }
