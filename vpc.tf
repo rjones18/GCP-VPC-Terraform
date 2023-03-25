adb45aed7b669cbfaadca07d2d410bbbbe3d57e9
rjones18 <regjones16@outlook.com>
2023-03-25 17:11:13 +0000

    Add private IP access

--- vpc.tf
+++ vpc.tf
@@ -22,6 +22,7 @@ resource "google_compute_subnetwork" "app-subnet-1" {
   ip_cidr_range = "10.0.10.0/24"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.self_link
+  private_ip_google_access = true
 }
 
 resource "google_compute_subnetwork" "app-subnet-2" {
@@ -29,6 +30,7 @@ resource "google_compute_subnetwork" "app-subnet-2" {
   ip_cidr_range = "10.0.11.0/24"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.self_link
+  private_ip_google_access = true
 }
 
 resource "google_compute_subnetwork" "data-subnet-1" {
@@ -36,6 +38,7 @@ resource "google_compute_subnetwork" "data-subnet-1" {
   ip_cidr_range = "10.0.20.0/24"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.self_link
+  private_ip_google_access = true
 }
 
 resource "google_compute_subnetwork" "data-subnet-2" {
@@ -43,6 +46,7 @@ resource "google_compute_subnetwork" "data-subnet-2" {
   ip_cidr_range = "10.0.21.0/24"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.self_link
+  private_ip_google_access = true
 }
 
 resource "google_compute_firewall" "web_firewall" {
