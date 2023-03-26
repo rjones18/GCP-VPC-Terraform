04e93c8484336bc6741147aed9a73edd55461846
rjones18 <regjones16@outlook.com>
2023-03-26 20:44:30 +0000

    Update Firewall rules

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,7 @@
 resource "google_compute_network" "vpc_network" {
   name                    = "project-vpc"
   auto_create_subnetworks = false
+
 }
 
 resource "google_compute_subnetwork" "web-subnet-1" {
@@ -68,28 +69,37 @@ resource "google_compute_firewall" "app_firewall" {
 
   allow {
     protocol = "tcp"
-    ports    = ["8080"]
+    ports    = ["22"]
   }
 
+  source_ranges = [ "0.0.0.0/0" ]
   source_tags = ["app"]
-  target_tags = ["db"]
 }
 
-resource "google_compute_firewall" "db_firewall" {
-  name    = "db-firewall"
-  network = google_compute_network.vpc_network.self_link
+# resource "google_compute_firewall" "db_firewall" {
+#   name    = "db-firewall"
+#   network = google_compute_network.vpc_network.self_link
 
-  allow {
-    protocol = "tcp"
-    ports    = ["3306"]
-  }
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["3306"]
+#   }
 
-  source_tags = ["app"]
-  target_tags = ["db"]
+#   source_ranges = [ "0.0.0.0/0" ]
+# #   target_tags = ["db"]
+# }
+
+
+resource "google_compute_global_address" "private_ip_address" {
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc_network.id
 }
 
 resource "google_service_networking_connection" "private_service_connection" {
-  network                 = google_compute_network.vpc_network.self_link
+  network                 = google_compute_network.vpc_network.id
   service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = ["10.0.10.0/24", "10.0.11.0/24", "10.0.20.0/24", "10.0.21/24"]
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
